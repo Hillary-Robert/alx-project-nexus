9e31f44120d121e5f9d6452a5af7584b32d006f3
Hillary Robert <roberthillary30@gmail.com>
2025-09-27 01:45:32 +0300

    Added viewport ffor mobile responsiveness

--- productify/pages/_app.tsx
+++ productify/pages/_app.tsx
@@ -1,5 +1,5 @@
-// pages/_app.tsx
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 import { Provider, useStore } from "react-redux";
 import { store } from "@/store";
@@ -11,7 +11,7 @@ import { hydrateCart, loadCart } from "@/store/cartSlice";
 function HydrateCartOnce() {
   const reduxStore = useStore();
   useEffect(() => {
-   
+ 
     reduxStore.dispatch(hydrateCart(loadCart()));
   }, [reduxStore]);
   return null;
@@ -20,13 +20,17 @@ function HydrateCartOnce() {
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+    
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
       <HydrateCartOnce />
+
       <div className="flex flex-col min-h-screen bg-gray-50 text-gray-900">
         <Header />
         <main className="flex-1">
-          <div>
-            <Component {...pageProps} />
-          </div>
+          <Component {...pageProps} />
         </main>
         <Footer />
       </div>
